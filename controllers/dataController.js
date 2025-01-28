ce47b17f1fc811d116bcac9cd81090e8ce5a4fb3
IvanTechCollect <idjuricic@techcollect.net>
2025-01-28 15:09:11 -0500

    update dataController.js

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -5,71 +5,77 @@ const CommunityRemittance = require('../models/CommunityRemittance');
 const CompanyRemittance = require('../models/CompanyRemittance');
 
 const extractData = async (unitId) => {
+    try {
+        const baseUrl = 'https://app.techcollect.net';
+        const foundUnit = await Unit.query().where('id', unitId).first();
+        const foundCompany = await Company.query().where('id', foundUnit.company_id).first();
+        const foundCommunity = await Community.query().where('id', foundUnit.community).first();
+        const subscribe_token = btoa(foundUnit.id.toString()); // Base64 encoding the unit_data.id
+        const unsubscribe_url = `${baseUrl}/api/unsubscribe/1/${subscribe_token}`; // Constructing the URL
 
-    const baseUrl = 'https://app.techcollect.net';
+        const remittance = await CommunityRemittance.query().where('community_id', foundUnit.community).first();
+        const companyRemittance = await CompanyRemittance.query().where('company_id', foundCompany.id);
 
-    const foundUnit = await Unit.query().where('id', unitId).first();
-    const foundCompany = await Company.query().where('id', foundUnit.company_id).first();
-    const foundCommunity = await Community.query().where('id', foundUnit.community).first();
-    const subscribe_token = btoa(foundUnit.id.toString()); // Base64 encoding the unit_data.id
-    const unsubscribe_url = `${baseUrl}/api/unsubscribe/1/${subscribe_token}`; // Constructing the URL
+        let remittanceName = '';
+        let remittanceAddress = '';
+        let remittanceCity = '';
+        let remittanceState = '';
+        let remittanceZip = '';
 
-    const remittance = await CommunityRemittance.query().where('community_id', foundUnit.community).first();
-    const companyRemittance = await CompanyRemittance.query().where('company_id', foundCompany.id);
+        if (foundUnit.company_id !== 287) {
+            remittanceName = remittance?.remittance_name || companyRemittance?.remittance_name || '';
+            remittanceAddress = remittance?.remittance_address || companyRemittance?.remittance_address || '';
+            remittanceCity = remittance?.remittance_city || companyRemittance?.remittance_city || '';
+            remittanceState = remittance?.remittance_state || companyRemittance?.remittance_state || '';
+            remittanceZip = remittance?.remittance_zip || companyRemittance?.remittance_zip || '';
+        }
 
-    let remittanceName = '';
-    let remittanceAddress = '';
-    let remittanceCity = '';
-    let remittanceState = '';
-    let remittanceZip = '';
+        const data = {
+            unsubscribe_url: unsubscribe_url,
+            unit_owner: foundUnit.unit_name,
+            community_name: foundCommunity.community_name,
+            date: new Date().toLocaleDateString('en-US'),
+            balance: foundUnit.delinquency_amount,
+            company_name: foundCompany.company_name,
+            company_email: foundCompany?.communication_email || null,
+            company_number: foundCompany.company_phone,
+            company_communication_phone_number: foundCompany.communication_phone,
+            company_address: foundCompany.address1,
+            company_city: foundCompany.city,
+            company_state: foundCompany.state,
+            company_zip: foundCompany.company_zip,
+            unit_address: foundUnit.address1,
+            unit_city: foundUnit.city,
+            unit_state: foundUnit.state,
+            unit_zip: foundUnit.zip_code,
+            account_number: foundUnit.accountNo,
+            payment_plan_price: '',
+            payment_plan_name: '',
+            payment_plan_next_due_date: '',
+            update_payment_method_url: '', // Will be updated once synapse finishes their work
+            mailing_address: foundUnit.mail_address || foundUnit.address1,
+            mailing_city: foundUnit.mail_city || foundUnit.city,
+            mailing_state: foundUnit.mail_state || foundUnit.state,
+            mailing_zip: foundUnit.mail_zip_code || foundUnit.zip_code,
+            community_communication_email: foundCommunity?.communication_email || foundCompany?.communication_email,
+            unit_email: foundUnit.email,
+            formal_date: new Date().toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' }),
+            remittance_address: remittanceAddress,
+            remittance_city: remittanceCity,
+            remittance_state: remittanceState,
+            remittance_zip: remittanceZip,
+            remittance_name: remittanceName,
+            assoc_code: foundCommunity.pield
+        };
 
-    if (foundUnit.company_id !== 287) {
-        remittanceName = remittance?.remittance_name || companyRemittance?.remittance_name || '';
-        remittanceAddress = remittance?.remittance_address || companyRemittance?.remittance_address || '';
-        remittanceCity = remittance?.remittance_city || companyRemittance?.remittance_city || '';
-        remittanceState = remittance?.remittance_state || companyRemittance?.remittance_state || '';
-        remittanceZip = remittance?.remittance_zip || companyRemittance?.remittance_zip || '';
+        return data;
+    } catch (error) {
+
+        console.log(error);
+        return null;
     }
 
-    const data = {
-        unsubscribe_url: unsubscribe_url,
-        unit_owner: foundUnit.unit_name,
-        community_name: foundCommunity.community_name,
-        date: new Date().toLocaleDateString('en-US'),
-        balance: foundUnit.delinquency_amount,
-        company_name: foundCompany.company_name,
-        company_email: foundCompany?.communication_email || null,
-        company_number: foundCompany.company_phone,
-        company_communication_phone_number: foundCompany.communication_phone,
-        company_address: foundCompany.address1,
-        company_city: foundCompany.city,
-        company_state: foundCompany.state,
-        company_zip: foundCompany.company_zip,
-        unit_address: foundUnit.address1,
-        unit_city: foundUnit.city,
-        unit_state: foundUnit.state,
-        unit_zip: foundUnit.zip_code,
-        account_number: foundUnit.accountNo,
-        payment_plan_price: '',
-        payment_plan_name: '',
-        payment_plan_next_due_date: '',
-        update_payment_method_url: '', // Will be updated once synapse finishes their work
-        mailing_address: foundUnit.mail_address || foundUnit.address1,
-        mailing_city: foundUnit.mail_city || foundUnit.city,
-        mailing_state: foundUnit.mail_state || foundUnit.state,
-        mailing_zip: foundUnit.mail_zip_code || foundUnit.zip_code,
-        community_communication_email: foundCommunity?.communication_email || foundCompany?.communication_email,
-        unit_email: foundUnit.email,
-        formal_date: new Date().toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' }),
-        remittance_address: remittanceAddress,
-        remittance_city: remittanceCity,
-        remittance_state: remittanceState,
-        remittance_zip: remittanceZip,
-        remittance_name: remittanceName,
-        assoc_code: foundCommunity.pield
-    };
 
-    return data;
 }
 
 const applyDataToTemplate = async (unitId, templateContent, subject = null) => {
@@ -77,6 +83,10 @@ const applyDataToTemplate = async (unitId, templateContent, subject = null) => {
 
     try {
         const data = await extractData(unitId);
+
+        if (!data) {
+            return { htmlContent: content }
+        }
 
         // Make a copy of the template content
         let content = templateContent;

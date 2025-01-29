df6f8ffc370a6a7faeb150dbf9ca93619c7d32e4
IvanTechCollect <idjuricic@techcollect.net>
2025-01-29 11:55:35 -0500

    update redis.js

--- config/redis.js
+++ config/redis.js
@@ -1,10 +1,10 @@
 const { createClient } = require('redis');
-
+const redisUrl = require('../config/redisConfig');
 const startRedis = async () => {
     // Connect to your internal Redis instance using the REDIS_URL environment variable
     // The REDIS_URL is set to the internal Redis URL e.g. redis://red-343245ndffg023:6379
     const client = createClient({
-        url: 'redis://default:kjhqwzzFQTHdqzrIkHPbzcXZPEAvDJiR@junction.proxy.rlwy.net:18442'
+        url: redisUrl
     });
 
     client.on('error', (err) => console.log('Redis Client Error', err));
